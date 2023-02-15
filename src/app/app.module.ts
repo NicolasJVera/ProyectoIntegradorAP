3c6f6e1d57a8a990b079e36101f5f0cc8ddfe83c
Nicolas Vera <nico03veraa@gmail.com>
2023-02-15 16:42:43 -0300

    creacion del dashboard para analizar la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { RegistroComponent } from './components/registro/registro.component';
     PortfolioComponent,
     FooterComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
